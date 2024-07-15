a2f013016fc1f13b4af8c85324e4a126b33521bc
Vivek Kumar <vivek3553.vr@gmail.com>
2024-07-15 20:48:09 +0530

    make footer responsive

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
 
   return (
     <footer className="bg-svg2 px-8 pt-4 text-gray-700">
-      <div className='flex justify-center gap-12 flex-wrap'>
+      <div className='flex justify-center gap-6 sm:gap-8 md:gap-12 flex-wrap'>
 
         <div className="flex flex-col items-center justify-center ">
           <img src={user} className='w-20 h-20 rounded-full object-cover shadow-2xl bw' alt="vivek-image" />
@@ -35,7 +35,7 @@ export default function Footer() {
             </Link>
           </div>
         </div>
-        <div className='flex gap-8 justify-center items-center'>
+        <div className='-mt-2 mb-2 flex gap-4 md:gap-8 justify-center items-center'>
           <Link className='hover:underline' to='/'>Home</Link>
           <Link className='hover:underline' to='/projects'>Projects</Link>
           <Link className='hover:underline' to='/contact'>Contact</Link>
